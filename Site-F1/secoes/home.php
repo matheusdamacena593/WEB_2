455c6402fbf04b888f636a2b94269f272509021f
matheusdamacena593 <mateusdamacena593@gmail.com>
2025-05-21 14:25:23 -0300

    Ajustar formatação e estrutura do código na seção de notícias

--- Site-F1/secoes/home.php
+++ Site-F1/secoes/home.php
@@ -1,22 +1,24 @@
 <?php
-    include_once("classes/ManipulaDados.php");
+include_once("classes/ManipulaDados.php");
 
-    $recebeNoticias = new ManipulaDados();
-    $recebeNoticias->setTable("tb_noticias");
-    $noticias = $recebeNoticias->getAllDataTable();
+$recebeNoticias = new ManipulaDados();
+$recebeNoticias->setTable("tb_noticias");
+$noticias = $recebeNoticias->getAllDataTable();
 
-    foreach ($noticias as $noticia) {
+foreach ($noticias as $noticia) {
 ?>
 
-<div class="card mt-5">
-    <div class="card-body">
-        <h5 class="card-title"><?=$noticia['titulo']?></h5>
-        <p class="card-text"><?=$noticia['descricao']?></p>
-        <p class="card-text"><small><?=$noticia['data']?> - <?=$noticia['autor']?></small></p>
+    <div class="container d-flex justify-content-center">
+        <div class="card mt-5" style="max-width: 600px; width: 100%;">
+            <div class="card-body">
+                <h5 class="card-title"><?= $noticia['titulo'] ?></h5>
+                <p class="card-text"><?= $noticia['descricao'] ?></p>
+                <p class="card-text"><small><?= $noticia['data'] ?> - <?= $noticia['autor'] ?></small></p>
+            </div>
+            <img class="card-img-bottom img-fluid mx-auto d-block" src="<?= $noticia['url'] ?>" alt="Noticia Atual" style="max-height: 300px; object-fit: cover;">
+        </div>
     </div>
-    <img class="card-img-bottom img-fluid w-50 mx-auto d-block" src="<?=$noticia['url']?>" alt="Noticia Atual"> 
-</div>
 
 <?php
-    }
+}
 ?>
